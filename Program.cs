85b3efdffe9d2ae33f13a5b0370bc831d0aad34b
NicolasMoreiraDev <ncmoreira2@gmail.com>
2025-08-05 18:37:00 -0300

    Implement Iphone and Nokia classes inheriting from Smartphone with overridden InstalarAplicativo method

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
+// Testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Smartphone nokia   = new Nokia("11-98877-6655", "Nokia", "123456789", 46);
+        Smartphone iphone  = new Iphone("11-97766-5544", "iPhone", "987654321", 64);
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+        nokia.Ligar();
+        nokia.InstalarAplicativo("LinkedIn");
+        nokia.ReceberLigacao();
+
+        Console.WriteLine();
+
+        iphone.Ligar();
+        iphone.InstalarAplicativo("GitHub");
+        iphone.ReceberLigacao();
+    }
+}
